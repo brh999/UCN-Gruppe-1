fd70b5f141d8918c837bb09cf5f100ff38dccccf
alpa59 <117630853+alpa59@users.noreply.github.com>
2022-11-08 13:10:56 +0100

    Update FriendCont.java
    getInstance() updated

--- src/model/FriendCont.java
+++ src/model/FriendCont.java
@@ -1,29 +1,34 @@
 package model;
+
 import java.util.*;
 
 public class FriendCont {
-	
+
 	private static FriendCont instance;
 	private static ArrayList<Friend> friends;
-	
-	public FriendCont() {
-		if(instance == null) {
+
+	public static FriendCont getInstance() {
+		if (instance == null) {
 			instance = new FriendCont();
-			instance.friends = new ArrayList<>();
 		}
+		return instance;
 	}
-	
+
+	private FriendCont() {
+		friends = new ArrayList<>();
+	}
+
 	public static Friend findFriendByPhone(String phone) {
 		Friend res = null;
-		for(Friend f : friends) {
-			if(f.getPhone().equals(res)) {
+		for (Friend f : friends) {
+			if (f.getPhone().equals(res)) {
 				res = f;
 			}
 		}
-		if(res == null) {
+		if (res == null) {
 			System.out.println("No friend found with phone: " + phone);
 		}
 		return res;
 	}
-	
+
 }

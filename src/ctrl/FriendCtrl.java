2b333e13fb8616451968ca19908a7fe3aa7b14e4
Vince <60509961+brh999@users.noreply.github.com>
2022-11-09 10:00:44 +0100

    Update

--- src/ctrl/FriendCtrl.java
+++ src/ctrl/FriendCtrl.java
@@ -1,12 +1,25 @@
 package ctrl;
 
 import model.FriendCont;
+import ctrl.*;
 import model.Friend;
+
+/**
+ * 
+ * @author Alex, Rasmus, Kasper, Dani (Gruppe 1)
+ *
+ */
 
 public class FriendCtrl {
 
+	/** Find friend by phone number
+	 * 
+	 * @param phone Phone number to search friend by
+	 * @return Friend
+	 */
 	public static Friend findFriendByPhone(String phone) {
 		return FriendCont.findFriendByPhone(phone);
 	}
+	
 	
 }
--- src/ctrl/LPCtrl.java
+++ src/ctrl/LPCtrl.java
@@ -1,17 +1,30 @@
 package ctrl;
 
 import model.*;
+/**
+ * 
+ * @author Rasmus, Alex, Kasper, Dani (Gruppe 1)
+ *
+ */
 
 public class LPCtrl {
 
 	public LPCtrl() {
 
 	}
-
+	
+	/**
+	 * Returns LP by title
+	 * 
+	 * @param title   Title of the LP
+	 * @return	LP
+	 */
+	
 	public static LP findLPByTitle(String title) {
 		LPCont lpc = LPCont.getInstance();
 		LP lp = lpc.findLPByTitle(title);
 		return lp;
 	}
+	
 
 }
